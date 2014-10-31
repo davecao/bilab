5575bd11a0c86d8a2cc28cfb97aa3f47cbb512c2
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2014-10-31 17:44:41 +0900

    Add parseDSSP

--- bilab/structure/pdbfile.py
+++ bilab/structure/pdbfile.py
@@ -18,7 +18,7 @@ from bilab import LOGGER, SETTINGS
 from bilab.structure.header import getHeaderDict, buildBiomolecules, assignSecstr
 #from .localpdb import fetchPDB
 
-__all__ = ['parsePDBStream', 'parsePDB', 'parsePQR', 'parseStride',
+__all__ = ['parsePDBStream', 'parsePDB', 'parsePQR', 'parseStride','parseDSSP',
            'writePDBStream', 'writePDB',]
 
 class PDBParseError(Exception):
@@ -991,4 +991,126 @@ def parseStride(stridefile, ag):
     ag.setData('stride_phi', PHI)
     ag.setData('stride_psi', PSI)
     ag.setData('stride_area', AREA)
+    return ag
+
+def parseDSSP(dssp, ag, parseall=False):
+    """Parse DSSP data from file *dssp* into :class:`~.AtomGroup` instance
+    *ag*.  DSSP output file must be in the new format used from July 1995
+    and onwards.  When *dssp* file is parsed, following attributes are added
+    to *ag*:
+
+    * *dssp_resnum*: DSSP's sequential residue number, starting at the first
+      residue actually in the data set and including chain breaks; this number
+      is used to refer to residues throughout.
+
+    * *dssp_acc*: number of water molecules in contact with this residue \*10.
+      or residue water exposed surface in Angstrom^2.
+
+    * *dssp_kappa*: virtual bond angle (bend angle) defined by the three Cα
+      atoms of residues I-2,I,I+2.  Used to define bend (structure code 'S').
+
+    * *dssp_alpha*: virtual torsion angle (dihedral angle) defined by the four
+      Cα atoms of residues I-1,I,I+1,I+2.Used to define chirality (structure
+      code '+' or '-').
+
+    * *dssp_phi* and *dssp_psi*: IUPAC peptide backbone torsion angles
+
+    The following attributes are parsed when ``parseall=True`` is passed:
+
+    * *dssp_bp1*, *dssp_bp2*, and *dssp_sheet_label*: residue number of first
+      and second bridge partner followed by one letter sheet label
+
+    * *dssp_tco*: cosine of angle between C=O of residue I and C=O of residue
+      I-1.  For α-helices, TCO is near +1, for β-sheets TCO is near -1.  Not
+      used for structure definition.
+
+    * *dssp_NH_O_1_index*, *dssp_NH_O_1_energy*, etc.: hydrogen bonds; e.g.
+      -3,-1.4 means: if this residue is residue i then N-H of I is h-bonded to
+      C=O of I-3 with an electrostatic H-bond energy of -1.4 kcal/mol.  There
+      are two columns for each type of H-bond, to allow for bifurcated H-bonds.
+
+    See http://swift.cmbi.ru.nl/gv/dssp/DSSP_3.html for details."""
+
+    if not os.path.isfile(dssp):
+        raise IOError('{0} is not a valid file path'.format(dssp))
+    if not isinstance(ag, AtomGroup):
+        raise TypeError('ag argument must be an AtomGroup instance')
+
+    dssp = open(dssp)
+
+    n_atoms = ag.numAtoms()
+    NUMBER = np.zeros(n_atoms, int)
+    SHEETLABEL = np.zeros(n_atoms, np.array(['a']).dtype.char + '1')
+    ACC = np.zeros(n_atoms, float)
+    KAPPA = np.zeros(n_atoms, float)
+    ALPHA = np.zeros(n_atoms, float)
+    PHI = np.zeros(n_atoms, float)
+    PSI = np.zeros(n_atoms, float)
+
+    if parseall:
+        BP1 = np.zeros(n_atoms, int)
+        BP2 = np.zeros(n_atoms, int)
+        NH_O_1 = np.zeros(n_atoms, int)
+        NH_O_1_nrg = np.zeros(n_atoms, float)
+        O_HN_1 = np.zeros(n_atoms, int)
+        O_HN_1_nrg = np.zeros(n_atoms, float)
+        NH_O_2 = np.zeros(n_atoms, int)
+        NH_O_2_nrg = np.zeros(n_atoms, float)
+        O_HN_2 = np.zeros(n_atoms, int)
+        O_HN_2_nrg = np.zeros(n_atoms, float)
+        TCO = np.zeros(n_atoms, float)
+
+    ag.setSecstrs(np.zeros(n_atoms, dtype=ATOMIC_FIELDS['secondary'].dtype))
+    for line in dssp:
+        if line.startswith('  #  RESIDUE'):
+            break
+    for line in dssp:
+        if line[13] == '!':
+            continue
+        res = ag[(line[11], int(line[5:10]), line[10].strip())]
+        if res is None:
+            continue
+        indices = res.getIndices()
+        res.setSecstrs(line[16].strip())
+        NUMBER[indices] = int(line[:5])
+        SHEETLABEL[indices] = line[33].strip()
+        ACC[indices] = int(line[35:38])
+        KAPPA[indices] = float(line[91:97])
+        ALPHA[indices] = float(line[97:103])
+        PHI[indices] = float(line[103:109])
+        PSI[indices] = float(line[109:115])
+
+        if parseall:
+            BP1[indices] = int(line[25:29])
+            BP2[indices] = int(line[29:33])
+            NH_O_1[indices] = int(line[38:45])
+            NH_O_1_nrg[indices] = float(line[46:50])
+            O_HN_1[indices] = int(line[50:56])
+            O_HN_1_nrg[indices] = float(line[57:61])
+            NH_O_2[indices] = int(line[61:67])
+            NH_O_2_nrg[indices] = float(line[68:72])
+            O_HN_2[indices] = int(line[72:78])
+            O_HN_2_nrg[indices] = float(line[79:83])
+            TCO[indices] = float(line[85:91])
+
+    ag.setData('dssp_resnum', NUMBER)
+    ag.setData('dssp_sheet_label', SHEETLABEL)
+    ag.setData('dssp_acc', ACC)
+    ag.setData('dssp_kappa', KAPPA)
+    ag.setData('dssp_alpha', ALPHA)
+    ag.setData('dssp_phi', PHI)
+    ag.setData('dssp_psi', PSI)
+
+    if parseall:
+        ag.setData('dssp_bp1', BP1)
+        ag.setData('dssp_bp2', BP2)
+        ag.setData('dssp_NH_O_1_index', NH_O_1)
+        ag.setData('dssp_NH_O_1_energy', NH_O_1_nrg)
+        ag.setData('dssp_O_NH_1_index', O_HN_1)
+        ag.setData('dssp_O_NH_1_energy', O_HN_1_nrg)
+        ag.setData('dssp_NH_O_2_index', NH_O_2)
+        ag.setData('dssp_NH_O_2_energy', NH_O_2_nrg)
+        ag.setData('dssp_O_NH_2_index', O_HN_2)
+        ag.setData('dssp_O_NH_2_energy', O_HN_2_nrg)
+        ag.setData('dssp_tco', TCO)
     return ag
