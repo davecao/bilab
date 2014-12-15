a41b63dd358d7987bca52e111f3266385148e8df
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2014-12-15 10:53:53 +0900

    seperate multithreaded class into Abstract..., ThreadTask, ThreadWorker, ThreadPool

--- bilab/concurrent/threadPool.py
+++ bilab/concurrent/threadPool.py
@@ -25,67 +25,16 @@ class ThreadPool(AbstractLocalThreadPool):
                                         WorkerCheckInterval=CheckInterval)
 
         self._next_thread_id = 0
-        #return
-        #self.tasks = Queue()
-        #self.resultsQueue = Queue()
 
-#        self.__threads = []
-#        self.__isJoining = False
-#        self.__resizeLock = Condition(Lock())
-#        self.__taskLock = Condition(Lock())
-#
-#        self.setThreadCount(num_threads)
+    def __keep_alive_thread_func(self):
+        self.tasks.join()
 
-#    def setThreadCount(self, num_threads):
-#        """
-#        Set the current pool size. Acquires the resizing lock,
-#        then calls the internal version to do real work
-#        """
-#        if self.__isJoining:
-#            return False
-#
-#        self.__resizeLock.acquire()
-#
-#        try:
-#            self.__setThreadCountNolock(num_threads)
-#        finally:
-#            self.__resizeLock.release()
-#
-#        return True
-#
-#    def __setThreadCountNolock(self, num_threads):
-#        """
-#        Set the current pool size, spawning or terminating threads
-#        if necessary. Internal use only; assume the resizing lock is held.
-#        """
-#
-#        while num_threads > len(self.__threads):
-#            newThread = Worker(self)
-#            self.__threads.append(newThread)
-#            #newThread.start()
-#
-#        # if need to shrink the pool, do so
-#        while num_threads < len(self.__threads):
-#            self.__threads[0].dismiss()
-#            del self.__threads[0]
-
-#    def getThreadCount(self):
-#        """ Return the number of threads in the pool """
-#        self.__resizeLock.acquire()
-#        try:
-#            return len(self.__threads)
-#        finally:
-#            self.__resizeLock.release()
-
-#    def dismissWokers(self, num_threads):
-#        """
-#        Tell num_threads worker threads to to quit when they're done.
-#        """
-#        for i in range(min(num_threads, self.__threads)):
-#            worker = self.workers.pop()
-#            worker.dismiss()
-#    def __keep_alive_thread_func(self):
-#        self.tasks.join()
+    def  __start_keep_alive_thread(self):
+        """ Keep alive threads """
+        self.keep_alive_thread = Thread(group=None, 
+                                    target=self.__keep_alive_thread_func, 
+                                    name="Thread_Pool keep alive thread")
+        self.keep_alive_thread.start()
 
     def add_worker_thread(self):
         """
@@ -99,26 +48,9 @@ class ThreadPool(AbstractLocalThreadPool):
         return w
 
     def add_task(self, taskid, func, *args, **kwargs):
-        #def add_task(self, func, *args, **kargs):
         """
         Add a task to the queue
         """
-        #callback = kwargs.pop("callback", None)
-        #self.tasks.put((func, args, kargs))
-#        if self.__isJoining == True:
-#            return False
-#
-#        if not callable(task):
-#            return False
-#
-#        self.__taskLock.acquire()
-#        try:
-#            #self.tasks.put((taskid, task, args, kwargs, callback))
-#            th_task = ThreadTask(task, args, kwargs)
-#            self.tasks.put((taskid, th_task))
-#            return True
-#        finally:
-#            self.__taskLock.release()
         th_task = ThreadTask(taskid, func, *args, **kwargs)
         self.tasks.put(th_task)
         self.add_threads_if_needed()
