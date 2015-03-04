7579b7dd9457d591c3e0f24232b8b402cd88a79c
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2015-03-04 16:43:48 +0900

    add an bool option altLoc to parseStride

--- bilab/structure/pdbfile.py
+++ bilab/structure/pdbfile.py
@@ -979,7 +979,7 @@ def parseStride(stridefile, ag, altLoc=False):
         if not line.startswith('ASG '):
             continue
         #res = ag[(line[9], int(line[10:15]), line[15].strip())]
-        try
+        try:
             """ 
             line[9]: chain id
             line[10:15]: residue number
