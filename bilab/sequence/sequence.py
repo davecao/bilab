9afc26da8f54a6d745dd4014934d3d436b31e3b2
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2015-03-19 14:25:21 +0900

    Finish integrating Sequence and Alphbet class

--- bilab/sequence/sequence.py
+++ bilab/sequence/sequence.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-"""This module defines the sequence class
+"""This module defines the sequence class, 
+    revised from weblogo library
 """
 import os
 import sys
@@ -8,6 +9,7 @@ import math
 import pprint
 import numpy as np
 
+from string import maketrans
 from weakref import WeakValueDictionary
 from types import *
 from bilab.aaprop import AAindex, AAindex1Parser
@@ -15,80 +17,207 @@ from bilab.sequence import Alphabet, generic_alphabet, protein_alphabet, nucleic
 
 __all__ = ['Sequence']
 
-class Sequence(object):
+class Sequence(str):
     """ 
-    Sequence class for single protein sequence
+    Sequence class for single protein sequence inherited from str class
 
     usage:
-
-    >>> seq = bilab.sequence.Sequence(header, letters, isAligned=False, alphabet=None)
-    >>> seq.id = 'test'
-    >>> seq.sequence = 'AAA'
-
+    >>> import bilab
+    >>> from bilab.sequence import Alphabet, generic_alphabet, protein_alphabet, nucleic_alphabet
+    >>> pr=bilab.sequence.Alphabet('ACSDGF')
+    >>> pr='ACSDGF'
+    >>> s1=bilab.sequence.Sequence(pr,name='test',alphabet=protein_alphabet)
+    >>> print(repr(s1))
+    >test
+    ACSDGF
     """
-#        Note: __slots__ works with instance variables
-    _instances = WeakValueDictionary()
-    #__slots__ = ['id', 'sequence', '__dict__']
-
-    @property
-    def Count(self):
-        return len(self._instances)
-
-#    def __new__(cls, obj, 
-#            alphabet= generic_alphabet, name=None,  description=None,
-#            ):
-#        self = str.__new__(cls, obj)
-#        if alphabet is None: 
-#            alphabet = generic_alphabet
-#        if  not isinstance(alphabet, Alphabet): 
-#            alphabet = Alphabet(alphabet)
-#        if not alphabet.alphabetic(self) :
-#            raise ValueError("Sequence not alphabetic %s, '%s'" %(alphabet, self))
-#        
-#        self._alphabet=alphabet
-#        self.name = name
-#        self.description = description
-#                           
-#        return self
- 
-    # BEGIN PROPERTIES
-            
-    # Make alphabet constant 
-    @property
-    def alphabet(self):
-        return self._alphabet
-
-    def __init__(self, 
-            name=None, 
-            description=None, 
-            alphabet=generic_alphabet,
-            isAligned=False):
-
-        super(Sequence, self).__init__()
-        # weak count
-        #print("{}".format(id(self)))
-        self._obj_id = id(self)
-        self.isAligned = isAligned
+    def __new__(cls, obj, 
+            alphabet= generic_alphabet, 
+            name=None,  
+            description=None,
+            isAligned=False
+            ):
+        self = str.__new__(cls, obj)
         if alphabet is None: 
             alphabet = generic_alphabet
         if  not isinstance(alphabet, Alphabet): 
             alphabet = Alphabet(alphabet)
         if not alphabet.alphabetic(self) :
             raise ValueError("Sequence not alphabetic %s, '%s'" %(alphabet, self))
-        self._alphabet = alphabet
+        self._alphabet=alphabet
         self.name = name
         self.description = description
+        self.isAligned = isAligned
+        return self
+ 
+    # BEGIN PROPERTIES
+    # Make alphabet constant 
+    @property
+    def alphabet(self):
+        return self._alphabet
+    # END PROPERTIES
 
-        self._instances[self._obj_id] = self
+    def ords(self):
+        """ Convert sequence into an array of integers
+        in the range [0, len(alphabet)]
+        """
+        return self._alphabet.ords(self)
 
-    def __str__(self):
-        return ">{0}\n{1}".format(self.name, self.alphabet.letters())
+    def tally(self, alphabet=None):
+        """ Counts the occurrences of alphabetic characters
+
+        Args:
+            alphabet -- an optional alternative alphabet
+
+        returns:
+            A list of charcter counts in alphabetic order
+        """
+        if not alphabet:
+            alphabet = self.alphabet
+        L = len(alphabet)
+        counts = [0,] * L
+        ords = alphabet.ords(self)
+        for n in ords:
+            if n<L:
+                counts[n] += 1
+        return counts
+
+    def __getslice__(self, i, j):
+        cls = self.__class__
+        return cls(str.__getslice__(self, i, j), self.alphabet)
+
+    def __getitem__(self, key):
+        cls = self.__class__
+        return ( str.__getitem__(self, key), self.alphabet)
+
+    def  __add__(self, other):
+        # self + other
+        cls = self.__class__
+        return cls(str.__add__(self,other), self.alphabet)
+
+    def __radd__(self, other):
+        # other + self, where other is a superclass of self
+        cls = self.__class__
+        return cls(str.__add__(self,other), self.alphabet)
+
+    def __eq__(self, other):
+        if not hasattr(other, "alphabet"):
+            return False
+        if self.alphabet != other.alphabet:
+            return False
+        return str.__eq__(self, other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+#    def __str__(self):
+#        """ self.name and alphabet will be not found when creating 
+#            an object. This is caused by:
+#       __new__  -  if not alphabet.alphabetic(self) : 
+#            where self is a string
+#       """
+#        return ">{0}\n{1}".format(self.name, self.alphabet.letters())
 
     def __repr__(self):
-        return self.__str__();
+        #return self.__str__();
+        return ">{0}\n{1}".format(self.name, self)
+#------- Not finished ------------------------
 
-    def __getitem__(self, pos):
-        return self._sequence[pos]
+    def join(self, string_list):
+        cls = self.__class__
+        return cls( super(Sequence, self).join(string_list), self.alphabet)
+
+    def reverse(self):
+        """ reverse an order of the sequence 
+            Return a new string
+        """
+        cls = self.__class__
+        return cls(self[::-1], self.alphabet)
+
+    def ungap(self):
+        return self.remove('-.~')
+
+    def remove(self, delchars):
+        """ Return a new sequence without specififed chars """
+        cls = self.__class__
+        cleanseq = ''.join(char for char in str(self)
+                           if char not in set(delchars))
+        return cls(cleanseq.translate(maketrans('', '')), self.alphabet)
+
+    def translator(self, frm='', to='', delete='', keep=None):
+        # Python Cookbook Recipe 1.9
+        # Chris Perkins, Raymond Hettinger
+        # usage:
+        #    >>std_aa_only = translator(keep="ACDEFGHILKMNPQRSTVWY")
+        #    >>std_aa_only("ADDGSFGHXXX")
+        #    ADDGSFGH  <--- XXX will be delete
+        #    >>
+        if len(to) == 1: to = to * len(frm)
+        trans = maketrans(frm, to)
+        if keep is not None:
+            allchars = maketrans('', '')
+            # delete is expanded to delete everything except
+            # what is mentioned in set(keep)-set(delete)
+            delete = allchars.translate(allchars, keep.translate(allchars, delete))
+        def translate(s):
+            return s.translate(trans, delete)
+        return translate
+
+    def lower(self):
+        """ Return a lowercase string """
+        cls = self.__class__
+        trans = maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')
+        return cls(str(self).translate(trans), self.alphabet)
+
+    def upper(self):
+        """ Return a upppercase string """
+        cls = self.__class__
+        trans = maketrans('abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        return cls(str(self).translate(trans), self.alphabet)
+
+    def mask(self, letters='abcdefghijklmnopqrstuvwxyz', mask='X') :
+        """Replace all occurrences of letters with the mask character.
+        The default is to replace all lower case letters with 'X'.
+        """
+        LL = len(letters)
+        if len(mask) !=1 : 
+            raise ValueError("Mask should be single character") 
+        to = mask * LL
+        trans = maketrans( letters, to)
+        cls = self.__class__
+        return cls(str(self).translate(trans), self.alphabet)
+
+    def words(self, k, alphabet=None):
+        """Return an iteration over all subwords of length k in the sequence. If an optional
+        alphabet is provided, only words from that alphabet are returned.
+        
+        >>> list(Sequence("abcabc").words(3))
+        ['abc', 'bca', 'cab', 'abc']
+        """
+        
+        if len(self) < k : 
+            return
+
+        # An optimization. Chopping up strings is faster.
+        seq = self.alphabet.normalize(self).tostring()
+        #seq = self.tostring() 
+    
+        for i in range(0,len(seq)-k+1) :
+            word = seq[i:i+k]
+            if alphabet is None or alphabet.alphabetic(word) :
+                yield word
+
+    def word_count(self, k, alphabet=None):
+        """ k-string continue string count 
+
+            Sequence("abcabc").word_count(3)
+            [('abc', 2), ('bca', 1), ('cab', 1)]
+        """
+        from itertools import groupby
+        words = sorted(self.words(k,alphabet))
+        return [ (item, sum( 1 for n in group) ) for item, group in groupby(words)]
+
 
 #    def __del__(self):
 #        """ Count references """
@@ -96,14 +225,6 @@ class Sequence(object):
 #            print 'Last Counter object deleted'
 #        else:
 #            print self.Count, ' Counter objects remaining'
-    def get_obj_id(self):
-        return self._obj_id
-
-    def get_id(self):
-        return self._id
-
-    def get_length(self):
-        return self._length
 
 #    def get_aa_freq(self):
 #        """ aa frequency """
@@ -143,8 +264,8 @@ class Sequence(object):
         """
         aa1prop = AAindex(parser = AAindex1Parser)
         numeric_rep = []
-        std_aa_only = translator(keep="ACDEFGHILKMNPQRSTVWY")
-        sequence = std_aa_only(self.alphabet.letters())
+        std_aa_only = self.translator(keep="ACDEFGHILKMNPQRSTVWY")
+        sequence = std_aa_only(self)
                 # check parameters:
         # if smooth is true and func should be given too
         if smooth and func is None:
