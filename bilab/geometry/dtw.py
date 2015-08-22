3cd40d58a09767bcf11f55eb33793fa4046bf172
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2015-08-22 15:06:01 +0900

    Add dtw and nth_element

---
+++ bilab/geometry/dtw.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+"""
+   Implementation of Dynamic Time Wraping
+"""
+import numpy as np
+import bilab
+
+def _validate_vector(u, dtype=None):
+    # XXX Is order='c' really necessary?
+    u = np.asarray(u, dtype=dtype, order='c').squeeze()
+    # Ensure values such as u=1 and u=[1] still return 1-D arrays.
+    u = np.atleast_1d(u)
+    if u.ndim > 1:
+        raise ValueError("Input vector should be 1-D.")
+    return u
+
+def _traceback(D):
+    """
+        traceback for DTW matrix
+    """
+    D = np.asarray(D, dtype=None, order='c').squeeze()
+    D = np.atleast_2d(D)
+    if D.ndim > 2:
+        raise ValueError("Input vector should be 2-D.")
+    wrap_path = [(0,0)]
+    i, j = np.array(D.shape) - 1
+    while (i > 0 and j > 0):
+        ind = np.argmin((D[i-1, j-1], D[i-1, j], D[i, j-1]))
+        if ind == 0:
+            i = i - 1
+            j = j - 1
+        elif ind == 1:
+            i = i - 1
+        elif ind == 2:
+            j = j - 1
+        wrap_path.append((i,j))
+    return wrap_path
+
+def stdDTW(u, v, dist=lambda x, y:abs(x-y)):
+    """
+    
+    Standard DTW algorithm with the complexity O(N^2)
+    
+    Args:
+        u (np array): 
+        v (np array):
+
+    Return:
+        distance and warp path between u and v
+    """
+    u = _validate_vector(u)
+    v = _validate_vector(v)
+    M = len(u) + 1
+    N = len(v) + 1
+    s = np.zeros([M, N])
+    s[:] = np.inf
+    s[0, 0] = 0
+    wrap_path = []
+    for i in range(1, M):
+        for j in range(1, N):
+            cost = abs(u[i-1] - v[j-1])
+            s[i, j] = cost + min(s[i-1, j],   # insertion
+                                 s[i, j-1],   # deletion
+                                 s[i-1, j-1]) # match
+    wrap_path = _traceback(s) # tuple
+    return (s[M-1, N-1]/sum(s.shape), wrap_path) 
+
+def ExpandedResWindow(lowResPath, shrinkU, shrinkV, X, Y, radius):
+    pass
+
+def constrainedDTW(u, v, window):
+    pass
+
+def fastDTW(u, v, radius=1):
+    """
+        Implementation of Fast DTW algorithm
+
+    Reference:
+    
+    Stan Salvador & Philip Chan, FastDTW: Toward Accurate Dynamic Time Warping 
+    in Linear Time and Space. KDD Workshop on Mining Temporal and 
+    Sequential Data, pp. 70-80, 2004
+    
+    Args:
+        u (np array): 
+        v (np array):
+
+    Return:
+        similarity distance and wrap path between u and v
+    """
+
+    u = _validate_vector(u)
+    v = _validate_vector(v)
+    M = len(u)
+    N = len(v)
+    minSize = radius + 2
+    resolution_factor = 2 # for shrinking data
+    if M < minSize or N < minSize:
+        # call Standard DTW
+        return stdDTW(u,v)
+    else:
+        # Recursive case 
+        # Project the warp path from a coarser resolution onto 
+        # the current current resolution. Run DTW only along the 
+        # projected path (and also ‘radius’ cells from the projected path).
+        shrinkU = u[:M/resolution_factor]
+        shrinkV = v[:N/resolution_factor]
+        lowResPath = fastDTW(shrinkU, shrinkV, radius)
+        #window = ExpandedResWindow(lowResPath, shrinkU, shrinkV, u, v, radius)
+
+        return constrainedDTW(u, v, window)
---
+++ bilab/utilities/nth_element.py
@@ -0,0 +1,91 @@
+
+""" function partition, _select, select 
+    for partial order
+    Ref http://rosettacode.org/wiki/Quickselect_algorithm#Python
+"""
+import random
+import numpy as np
+
+__all__ = ["nth_element"]
+
+def medianOf3(vector, a, b, c, comp):
+    #comp a comparator, i.e. a boolean function accepting two parameters a and b,
+    #        and returning true if a < b and false if a >= b.
+    # in C or javascript
+    # comp (A, B) ?
+    #   comp (B, C) ? b : comp (A, C) ? c : a :
+    #       comp (A, C) ? a : comp (B, C) ? c : b;
+    A = vector[a]
+    B = vector[b]
+    C = vector[c]
+    if comp(A, B):
+        # A < B
+        if comp(B, C):
+            # A < B < C
+            return b
+        else:
+            # A<B, C<B
+            if comp(A, C):
+                # A < C < B
+                return c
+            else:
+                # A<B, A>C, B>C -> B > A > C
+                return a
+    else:
+        # A > B
+        if comp(A, C):
+            # C > A > B 
+            return a
+        else:
+            # A>B and A> C
+            if comp(B, C):
+                #A>B B<C A>C -> A>C>B
+                return c
+            else:
+                # A>B A>C B>C -> A>B>C
+                return b
+
+def partition(vector, left, right, pivotIndex, comp):
+    pivotValue = vector[pivotIndex]
+    # Move pivot to end
+    vector[pivotIndex], vector[right] = vector[right], vector[pivotIndex]  
+    storeIndex = left
+    for i in range(left, right):
+        #if vector[i] < pivotValue:
+        if comp(vector[i], pivotValue):
+            vector[storeIndex], vector[i] = vector[i], vector[storeIndex]
+            storeIndex += 1
+    # Move pivot to its final place
+    vector[right], vector[storeIndex] = vector[storeIndex], vector[right]  
+    return storeIndex
+ 
+def _select(vector, left, nth, right, comp):
+    "Returns the n-th smallest, (nth >= 0), element of vector within vector[left:right+1] inclusive."
+    while True:
+        # select pivotIndex between left and right
+        #pivotIndex = random.randint(left, right)
+        # meadian (left+rigth)>>1
+        pivotIndex = medianOf3(vector, left, right, (left + right)>>1, comp)
+        pivotNewIndex = partition(vector, left, right, pivotIndex, comp)
+        pivotDist = pivotNewIndex - left
+        if pivotDist == k:
+            return vector[pivotNewIndex]
+        elif k < pivotDist:
+            right = pivotNewIndex - 1
+        else:
+            k -= pivotDist + 1
+            left = pivotNewIndex + 1
+
+def nth_element(vector, left, nth, right, comp):
+    """ Return the k-th smallest, (k >= 0), element of vector within vector[left:right+1].
+    left, right default to (0, len(vector) - 1) if omitted
+    """
+    if left is None:
+        left = 0
+    lv1 = len(vector) - 1
+    if right is None:
+        right = lv1
+    assert vector and nth >= 0, "Either null vector or k < 0 "
+    assert 0 <= left <= lv1, "left is out of range"
+    assert left <= right <= lv1, "right is out of range"
+    return _select(vector, left, nth, right, comp)
