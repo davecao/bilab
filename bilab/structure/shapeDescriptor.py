11f3294e92352a22e1fad1c5282283ffe8d0760d
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2015-09-02 18:56:58 +0900

    Add Heap implementations and unfinished shapeDescriptor

---
+++ bilab/structure/shapeDescriptor.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['descriptor']
+
+
+def descriptor(mol):
+    """ 3D shape descriptor """
+    """
+    Args:
+        mol (class)        : bilab.structure.AtomGroup
+        cutoff (float)     : cutoff of distance for a residue 
+                             to generate a neighbor list
+        selection (string) : the atom name for selection, 
+                            default is CA of a residue.
+
+    Returns:
+        list of 3d shape descriptor of the molecule(typically, CA).
+    """
+    
