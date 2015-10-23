7d1a4a8589874d64bbf2f0da3d3e212ded467c67
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2015-10-23 17:38:48 +0900

    Coding read Amber params file

--- bilab/structure/forcefields/FFDataParser.py
+++ bilab/structure/forcefields/FFDataParser.py
@@ -6,6 +6,17 @@ from functools import wraps
 from bilab.io.FortranFormat import FortranFormat, FortranLine
 from bilab import PY3K
 from bilab import forcefieldsList
+from bilab.structure.forcefields import AmberAtomType, AmberBondParameters, \
+            AmberBondAngleParameters, AmberDihedralParameters, \
+            AmberImproperParameters,  AmberHbondParameters,\
+            AmberLJParameterSet
+
+# Replace enumrate with i, item in zip(range(len(foo)), foo)
+if PY3K:
+    from itertools import zip 
+else:
+    from itertools import izip as zip
+
 
 __all__ = ['FFParserInterface', 'AmberParamParser']
 
@@ -97,6 +108,35 @@ class FFParserInterface(object):
 class AmberParamParser(FFParserInterface):
     """
         Parser for Amber Force Field parameter file
+
+    Amber Params structure: in Order 
+        Title:  one line
+        1. AtomType
+            FortranFormat('A2,1X,F10.2')
+            FortranLine(file.readline()[:-1], format)
+            -- blank line --
+        2. Hydrophylic atom types
+            FortranFormat('20(A2,2X)') <-- Hydrophylic atom 
+            -- blank line --
+        3. BondParameters
+            FortranFormat('A2,1X,A2,2F10.2')
+            -- blank line --
+        4. AngleParameters (bond angles)
+            FortranFormat('A2,1X,A2,1X,A2,2F10.2')
+            -- blank line --
+        5. DihedralParameters
+            FortranFormat('A2,1X,A2,1X,A2,1X,A2,I4,3F15.2')
+            -- blank line --
+        6. ImproperParamters
+            FortranFormat('A2,1X,A2,1X,A2,1X,A2,I4,3F15.2')
+            -- blank line --
+        7. HydrongenBondParameters
+            FortranFormat('2X,A2,2X,A2,2X,2F10.2')
+            -- blank line --
+        8. Lennard-Jones Parameters
+            FortranFormat('2X,A2,6X,3F10.6')
+            -- blank line --
+        END
     """
     def __init__(self, *args, **kwargs):
         super(AmberParamParser, self).__init__()
@@ -108,4 +148,80 @@ class AmberParamParser(FFParserInterface):
         return self.__str__()
 
     def parse(self, filename):
-        print("parse file {}".format(filename))
+
+        AtomType, Hydrophilic, Bond, BondAngle, Dihedral, ImproperParams, HydrongenBond, LJParams = range(8)
+        state_ = -1
+        title = ""
+        atomType_dict = {}
+        bond_dict = {}
+        bondangle_dict = {}
+        dihedral_dict = {}
+        impropParams_arr = {}
+        hbond_arr = {}
+        ljParams_arr = {}
+        formats = [
+            FortranFormat('A2,1X,F10.2'),
+            FortranFormat('20(A2,2X)'),
+            FortranFormat('A2,1X,A2,2F10.2'),
+            FortranFormat('A2,1X,A2,1X,A2,2F10.2'),
+            FortranFormat('A2,1X,A2,1X,A2,1X,A2,I4,3F15.2'),
+            FortranFormat('A2,1X,A2,1X,A2,1X,A2,I4,3F15.2'),
+            FortranFormat('2X,A2,2X,A2,2X,2F10.2'),
+            FortranFormat('2X,A2,6X,3F10.6')
+        ]
+        with open(filename, 'r') as fhandle:
+            # line by line
+            line_format = formats[0]
+            #for lineno, line in zip(range(len(fhandle)), fhandle):
+            for line in fhandle:
+                if line.isspace():
+                    state_ += 1
+                    line_format = formats[state_]
+                    continue
+
+                if state_ == -1 :
+                    # read title 
+                    title = line.strip()
+                    state_ = AtomType
+
+                if state_ == AtomType:
+                    con = FortranLine(line.strip(), line_format)
+                    #atomType_arr.append(AmberAtomType(con[0].strip(), con[1]))
+                    atomType_dict[con[0].strip()] = \
+                            AmberAtomType(con[0].strip(), con[1])
+
+                if state_ == Hydrophylic:
+                    con = FortranLine(line.strip(), line_format)
+                    for at in con:
+                        atomType_dict[at.strip()].hydrophylic = True
+
+                if state_ == Bond:
+                    con = FortranLine(line.strip(), line_format)
+                    name1, name2 = sorted(con[0].strip(), con[1].strip())
+                    bond_dict[(name1, name2)] = \
+                        AmberBondParameters(atomType_dict[name1],
+                                            atomType_dict[name2],
+                                            con[2], con[3])
+                if state_ == BondAngle:
+                    con = FortranLine(line.strip(), line_format)
+                    name1, name2, name3 = sorted([con[0].strip(), 
+                                                  con[1].strip(), 
+                                                  con[2].strip()])
+                    bondangle_dict[(name1, name2, name3)] = \
+                        AmberBondAngleParameters(atomType_dict[name1],
+                                                 atomType_dict[name2],
+                                                 atomType_dict[name3],
+                                                 con[3].strip(),
+                                                 con[4].strip())
+                if state_ == Dihedral:
+                    con = FortranLine(line.strip(), line_format)
+                    name1, name2, name3, name4 = sorted([con[0].strip(), 
+                                                  con[1].strip(), 
+                                                  con[2].strip(),
+                                                  con[3].strip()])
+
+                    p = AmberDihedralParameters(self.atom_types[name1],
+                                            self.atom_types[name2],
+                                            self.atom_types[name3],
+                                            self.atom_types[name4],
+                                            con[4], con[5], con[6], con[7])
--- bilab/structure/forcefields/ForceField.py
+++ bilab/structure/forcefields/ForceField.py
@@ -8,7 +8,13 @@ from bilab import PY3K
 from bilab import forcefieldsList
 from bilab.structure.forcefields import AmberParamParser
 
-__all__ = ['ForceField', 'AmberForceField']
+__all__ = ['ForceField', 'AmberForceField', 
+            'AmberAtomType', 'AmberBondParameters',
+            'AmberBondAngleParameters',
+            'AmberDihedralParameters',
+            'AmberImproperParameters',
+            'AmberHbondParameters',
+            'AmberLJParameterSet']
 
 # decorator borrowed from Mozilla mxr
 def abstractmethod(method):
@@ -103,6 +109,113 @@ class ForceField(object):
                 #cls.__metaclass__.registry[name]
         except ValueError:
             raise ValueError("{} is not registered yet or wrong name".format(name))
+
+class AmberAtomType(object):
+    """ Atom Type """
+    def __init__(self, name, mass):
+        self.name = name
+        self.mass = mass
+        self.hydrophlic = False
+
+class AmberBondParameters(object):
+    """ Bond  """
+    def __init__(self, a1, a2, k, l):
+        self.a1 = a1
+        self.a2 = a2
+        self.k = k
+        self.l = l
+
+#
+# Bond angle parameter class
+#
+class AmberBondAngleParameters(object):
+
+    def __init__(self, a1, a2, a3, k, angle):
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.k = k
+        self.angle = angle
+
+#
+# Dihedral angle parameter class
+#
+class AmberDihedralParameters(object):
+
+    def __init__(self, a1, a2, a3, a4, divf, k, delta, n):
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.terms = [(k/divf, delta, int(abs(n)))]
+
+    def addTerm(self, divf, k, delta, n):
+        self.terms.append((k/divf, delta, int(abs(n))))
+
+    def __repr__(self):
+        if self.a1 is None and self.a4 is None:
+            return 'X-' + self.a2.name + '-' + self.a3.name + '-X'
+        else:
+            return self.a1.name + '-' + self.a2.name + '-' + \
+                   self.a3.name + '-' + self.a4.name
+    __str__ = __repr__
+
+#
+# Improper dihedral angle parameter class
+#
+class AmberImproperParameters(object):
+
+    def __init__(self, a1, a2, a3, a4, divf, k, delta, n):
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.terms = [(0.5*k, delta, int(abs(n)))]
+
+    def addTerm(self, divf, k, delta, n):
+        self.terms.append((0.5*k, delta, int(abs(n))))
+
+    def __repr__(self):
+        if self.a4 is None:
+            if self.a3 is None:
+                return self.a1.name + '-' + self.a2.name + '-X-X'
+            else:
+                return self.a1.name + '-' + self.a2.name + '-' + \
+                       self.a3.name + '-X'
+        else:
+            return self.a1.name + '-' + self.a2.name + '-' + \
+                   self.a3.name + '-' + self.a4.name
+    __str__ = __repr__
+
+#
+# H-bond parameter class
+#
+class AmberHbondParameters(object):
+
+    def __init__(self, a1, a2, a, b):
+        self.a1 = a1
+        self.a2 = a2
+        self.a = a
+        self.b = b
+
+#
+# Lennard-Jones parameter sets
+#
+class AmberLJParameterSet(object):
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.entries = {}
+
+    def addEntry(self, name, p1, p2, p3):
+        if self.type == 'SK':
+            self.entries[name] = (p1, p2, p3)
+        else:
+            self.entries[name] = (p1, p2)
+
+    def getEntry(self, name):
+        return self.entries[name]
 
 class AmberForceField(ForceField):
     """
