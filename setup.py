3f6e01022b034a584f49172019df743999947669
Wei Cao <davecao@bi.a.u-tokyo.ac.jp>
2017-06-05 14:58:54 +0900

    Unicode causes the TypeError if python version less than 2.7.12

--- setup.py
+++ setup.py
@@ -525,7 +525,7 @@ def split_multiline(value):
     """Split a multiline string into a list, excluding blank lines."""
     
     if sys.version_info[:3] < (2, 7, 13):
-        val = value.encode('ascii', 'ignore')
+        val = value.decode('ascii', 'ignore')
         return [element for element in
                 (line.strip() for line in value.split('\n'))
                 if element]
